36b0cd5a6f62dc0306241c50d9820267b8c8ab1a
rahul sinha <rahulsinha.kvs@gmail.com>
2021-06-11 21:46:38 +0530

    <changes that has been added>

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,8 +1,37 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  const [todos, setTodos] = useState(["Go to sleep", "wake up", "awake"]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (event) => {
+    event.preventDefault(); //stops refresh
+    console.log("Im Working");
+    setTodos([...todos, input]);
+    setInput(""); //clear the input after hitting submit
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <Button
+        type="submit"
+        onClick={addTodo}
+        variant="contained"
+        color="primary"
+      >
+        Add Todo
+      </Button>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
