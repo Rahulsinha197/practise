7c191054df39c9a8d41dadd845e24609492d9d10
Rahulsinha197 <rahul.s@simformsolutions.com>
2021-09-16 16:39:04 +0530

    feat: implementing hoc and render props

---
+++ redux-mui-crud/src/pages/Visit.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import HoverCounterTwo from "../components/HoverCounterTwo";
+import ClickCounterTwo from "../components/ClickCounterTwo";
+import User from "../components/User";
+import Counter from "../components/Counter";
+
+export default class Visit extends Component {
+  render() {
+    return (
+      <div>
+        <Counter
+          render={(count, incrementCount) => (
+            <ClickCounterTwo count={count} incrementCount={incrementCount} />
+          )}
+        />
+
+        <Counter
+          render={(count, incrementCount) => (
+            <HoverCounterTwo count={count} incrementCount={incrementCount} />
+          )}
+        />
+
+        {/* <div>
+          <ClickCounterTwo />
+        </div>
+        <div>
+          <HoverCounterTwo />
+        </div>
+        <div>
+          <User render={(isLoggedIn) => (isLoggedIn ? "Rahul" : "Anonymous")} />
+        </div> */}
+      </div>
+    );
+  }
+}
